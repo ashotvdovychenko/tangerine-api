555fdb6616c7b481f2f70d7cccffe99e91ba5421
Ashot Vdovychenko <ashotvdovichenko@gmail.com>
2023-10-27 23:26:18 +0300

    Fix checkstyle

--- src/main/java/com/example/tangerine/api/TangerineApiApplication.java
+++ src/main/java/com/example/tangerine/api/TangerineApiApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TangerineApiApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(TangerineApiApplication.class, args);
-    }
-
+  public static void main(String[] args) {
+    SpringApplication.run(TangerineApiApplication.class, args);
+  }
 }
