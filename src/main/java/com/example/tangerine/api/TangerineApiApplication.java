48a4392caf918d45688608ac3aac53e93ba35130
Ashot Vdovychenko <ashotvdovichenko@gmail.com>
2023-10-27 23:08:29 +0300

    Init commit

---
+++ src/main/java/com/example/tangerine/api/TangerineApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.tangerine.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TangerineApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TangerineApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tangerine/api/TangerineApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tangerine.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TangerineApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
